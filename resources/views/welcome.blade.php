2a0ee49704835142401e89f2ec40b7c979cbc92c
ilyasistiqama <ilyasistiqama@gmail.com>
2020-12-24 16:01:28 +0700

    coba gan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    HUHUHUHUHUHHUHUHU
                 </div>
 
                 <div class="links">
